b57c9df4d41100c027a98217c9d49e5502b5fef5
MangataTS <1196991321@qq.com>
2022-03-02 16:48:15 +0800

    添加百度统计

--- docs/javascripts/baidu-tongji.js
+++ docs/javascripts/baidu-tongji.js
@@ -1,3 +1,4 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -5,3 +6,4 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
