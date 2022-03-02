404c1612758ae2ff9070c0015660a047eddd40c1
MangataTS <1196991321@qq.com>
2022-03-02 16:34:55 +0800

    V0.8

---
+++ docs/javascripts/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a10af5fa98859b6cd421d58c9d2a414e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
