778ce9407200b9e8ea13528d9673b9b47cd507c2
MangataTS <1196991321@qq.com>
2022-03-02 22:18:44 +0800

    新增右下角聊天留言功能

---
+++ docs/javascripts/cheat.js
@@ -0,0 +1,10 @@
+(function(d, w, c) {
+	w.ChatraID = 'XXai8SKMvXTbd8sXy';
+	var s = d.createElement('script');
+	w[c] = w[c] || function() {
+		(w[c].q = w[c].q || []).push(arguments);
+	};
+	s.async = true;
+	s.src = 'https://call.chatra.io/chatra.js';
+	if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
