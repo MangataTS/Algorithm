0fa88947b780743c0762224e9d7ed050e3ef0f84
MangataTS <1196991321@qq.com>
2022-09-18 20:16:57 +0800

    删除百度网站统计

--- docs/javascripts/baidu-tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?a10af5fa98859b6cd421d58c9d2a414e";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
